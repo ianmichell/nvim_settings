7cec525c1a737771cfe2a6640cbed6c75c88db8d
Ian Michell <ian.michell@oneka.co.uk>
2023-03-28 06:58:04 +0100

    fixing issue with nvim

--- lua/settings/packer.lua
+++ lua/settings/packer.lua
@@ -104,6 +104,6 @@ return require('packer').startup(function(use)
     -- Scala plugin, not sure I want this yet  --
     use({'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" }})
 
-    -- Rust specific --
+    -- Debugging --
     use('mfussenegger/nvim-dap')
 end)
